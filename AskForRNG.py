818d888da095854a3b85b41b976bc81fe5df5101
Peter <87620801+1965pallison@users.noreply.github.com>
2023-12-02 13:57:36 -0500

    Completed RNG, improvements later?

--- AskForRNG.py
+++ AskForRNG.py
@@ -1,13 +1,27 @@
-#AskForRNG
+#AskForRNG Project
+#
+# Project Objectives
+#   ask for input and do somthing with it 
+#   if (else) and while conditions
+#   
+# TO-DO/Improvements
+#   create specific parameters for wrong input
+#   
 
 
-#Variables
 
 
+#Input
+RNGprompt = str(input("Would you like a random number? respond with y or n \n  "))
 
-#Process
-
-
-
+#Actual Random Number Generator 
+import random
+x = random.randint(1, 100)
 
 #Output
+while RNGprompt == "y":
+    print("Okay! Here's a random number:",x)
+    RNG = str(input("Would you like another random number?"))
+    x = random.randint(1, 100)
+else:
+    print("Aww okay...")
